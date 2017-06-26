d3084e31545d9e33c5f5ddea26abe3646989c541
Terrence Wong <wongterrencew@gmail.com>
2017-06-26 23:37:44 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,135 @@
+const forEach = require('lodash.foreach')
+const OPERATION_METHODS = ["get", "put", "post", "delete", "options", "head", "patch"]
+const DEFAULT_TAG = 'default'
+
+class Schema {
+  constructor (spec) {
+    this.spec = spec
+  }
+
+  info () {
+    return this.spec.info
+  }
+
+  version () {
+    return this.spec.info.version
+  }
+
+  paths () {
+    return this.spec.paths
+  }
+
+  operations () {
+    const paths = this.paths()
+
+    if (!paths || !Object.keys(paths)) return []
+
+    let list = []
+
+    forEach(paths, (path, pathName) => {
+      forEach(path, (operation, method) => {
+        if (OPERATION_METHODS.indexOf(method) === -1) {
+          return
+        }
+
+        list.push({
+          path: pathName,
+          method,
+          operation,
+          id: `${method}-${pathName}`
+        })
+      })
+    })
+
+    return list
+  }
+
+  operationsWithRootInherited () {
+    const operations = this.operations()
+    const consumes = this.consumes()
+    const produces = this.produces()
+
+    return operations.map(operationObject => {
+      const { operation } = operationObject
+      if (!operation.consumes) operation.consumes = consumes
+      if (!operation.produces) operation.produces = produces
+      return Object.assign({}, operationObject, { operation })
+    })
+  }
+
+  operationsWithTags () {
+    const operations = this.operationsWithRootInherited()
+    return operations.reduce((taggedMap, operationObject) => {
+      const tags = operationObject.operation.tags || []
+      if (!tags.length) {
+        taggedMap[DEFAULT_TAG]
+          ? taggedMap[DEFAULT_TAG].push(operationObject)
+          : taggedMap[DEFAULT_TAG] = [operationObject]
+      }
+
+      return tags.reduce((result, tag) => {
+        result[tag]
+          ? result[tag].push(operationObject)
+          : result[tag] = [operationObject]
+        return result
+      }, taggedMap)
+    }, {})
+  }
+
+  tagDetails (tag) {
+    const tags = this.tags()
+    return tags.find(({name}) => name === tag)
+  }
+
+  taggedOperations () {
+    const taggedMap = this.operationsWithTags()
+
+    forEach(taggedMap, (operations, tag) => {
+      taggedMap[tag] = {
+        tagDetails: this.tagDetails(tag),
+        operations
+      }
+    })
+
+    return taggedMap
+  }
+
+  consumes () {
+    return this.spec.consumes
+  }
+
+  produces () {
+    return this.spec.produces
+  }
+
+  security () {
+    return this.spec.security
+  }
+
+  securityDefinitions () {
+    return this.spec.securityDefinitions
+  }
+
+  definitions () {
+    return this.spec.definitions
+  }
+
+  basePath () {
+    return this.spec.basePath
+  }
+
+  host () {
+    return this.spec.host
+  }
+
+  schemes () {
+    return this.spec.schemes
+  }
+
+  tags () {
+    return this.spec.tags
+  }
+
+}
+
+module.exports = Schema
---
+++ test.js
@@ -0,0 +1,176 @@
+const Schema = require('.')
+
+describe('swagger-parser-js', () => {
+  it('operations', () => {
+    const schema = {
+      paths: {
+        '/pet': {
+          post: {
+            summary: 'Add a new pet to the store'
+          }
+        }
+      }
+    }
+
+    expect(new Schema(schema).operations()).toEqual([
+      {
+        id: 'post-/pet',
+        method: 'post',
+        operation: { summary: 'Add a new pet to the store' },
+        path: '/pet'
+      }
+    ])
+  })
+
+  it('operationsWithRootInherited', () => {
+    const schema = {
+      consumes: 'consumes',
+      produces: 'produces',
+      paths: {
+        '/pet': {
+          post: {
+            summary: 'Add a new pet to the store'
+          }
+        }
+      }
+    }
+
+    expect(new Schema(schema).operationsWithRootInherited()).toEqual([
+      {
+        id: 'post-/pet',
+        method: 'post',
+        operation: {
+          summary: 'Add a new pet to the store',
+          consumes: 'consumes',
+          produces: 'produces'
+        },
+        path: '/pet'
+      }
+    ])
+  })
+
+  it('operationsWithTags', () => {
+    const schema = {
+      consumes: 'consumes',
+      produces: 'produces',
+      paths: {
+        '/pet': {
+          post: {
+            summary: 'Add a new pet to the store',
+            tags: ['pets']
+          }
+        },
+        '/hipster-things': {
+          post: {
+            summary: 'some hipster stuff',
+            tags: ['carbon-neutral']
+          }
+        }
+      }
+    }
+
+    expect(new Schema(schema).operationsWithTags()).toEqual({
+      'pets': [{
+        id: 'post-/pet',
+        method: 'post',
+        operation: {
+          summary: 'Add a new pet to the store',
+          tags: ['pets'],
+          consumes: 'consumes',
+          produces: 'produces'
+        },
+        path: '/pet'
+      }],
+      'carbon-neutral': [{
+        id: 'post-/hipster-things',
+        method: 'post',
+        operation: {
+          summary: 'some hipster stuff',
+          tags: ['carbon-neutral'],
+          consumes: 'consumes',
+          produces: 'produces'
+        },
+        path: '/hipster-things'
+      }]
+    })
+  })
+
+  it('tagDetails', () => {
+    const tagDetails = {
+      name: 'pets',
+      description: 'pets are the best!'
+    }
+    const schema = {
+      tags: [tagDetails]
+    }
+    expect(new Schema(schema).tagDetails('pets')).toBe(tagDetails)
+  })
+
+  it('taggedOperations', () => {
+    const schema = {
+      tags: [
+        {
+          name: 'pets',
+          description: 'pets are the best!'
+        },
+        {
+          name: 'carbon-neutral',
+          description: 'carbon neutral is also the best!'
+        }
+      ],
+      consumes: 'consumes',
+      produces: 'produces',
+      paths: {
+        '/pet': {
+          post: {
+            summary: 'Add a new pet to the store',
+            tags: ['pets']
+          }
+        },
+        '/hipster-things': {
+          post: {
+            summary: 'some hipster stuff',
+            tags: ['carbon-neutral']
+          }
+        }
+      }
+    }
+
+    expect(new Schema(schema).taggedOperations()).toEqual({
+      'pets': {
+        tagDetails: {
+          name: 'pets',
+          description: 'pets are the best!'
+        },
+        operations: [{
+          id: 'post-/pet',
+          method: 'post',
+          operation: {
+            summary: 'Add a new pet to the store',
+            tags: ['pets'],
+            consumes: 'consumes',
+            produces: 'produces'
+          },
+          path: '/pet'
+        }],
+      },
+      'carbon-neutral': {
+        tagDetails: {
+          name: 'carbon-neutral',
+          description: 'carbon neutral is also the best!'
+        },
+        operations: [{
+          id: 'post-/hipster-things',
+          method: 'post',
+          operation: {
+            summary: 'some hipster stuff',
+            tags: ['carbon-neutral'],
+            consumes: 'consumes',
+            produces: 'produces'
+          },
+          path: '/hipster-things'
+        }]
+      }
+    })
+  })
+})
